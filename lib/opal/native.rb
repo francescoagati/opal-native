f406c0e1ec9bb6ee20e20a8b1df2168a52fc80bb
meh <meh@paranoici.org>
2012-01-18 21:27:38 +0100

    Bunch of improvements

--- lib/opal/native.rb
+++ lib/opal/native.rb
@@ -10,12 +10,12 @@
 
 class Module
 	%x{
-		function define_attr_bridge(klass, target, name, getter, setter) {
+		function define_attr_bridge (klass, target, name, getter, setter) {
 			if (getter) {
 				$opal.defn(klass, $opal.mid_to_jsid(name), function() {
 					var real_target = target;
 
-					if (target.$f & T_STRING) {
+					if (#{Symbol == target}) {
 						real_target = target[0] == '@' ? this[target.substr(1)] : this[$opal.mid_to_jsid(target)].apply(this, null);
 					}
 
@@ -29,7 +29,7 @@ class Module
 				$opal.defn(klass, $opal.mid_to_jsid(name + '='), function (block, val) {
 					var real_target = target;
 
-					if (target.$f & T_STRING) {
+					if (#{Symbol === target}) {
 						real_target = target[0] == '@' ? this[target.substr(1)] : this[$opal.mid_to_jsid(target)].apply(this, null);
 					}
 
@@ -59,7 +59,7 @@ class Module
 		self
 	end
 
-	def attr_reader_bridge (target, *attrs)
+	def attr_writer_bridge (target, *attrs)
 		%x{
 			for (var i = 0, length = attrs.length; i < length; i++) {
 				define_attr_bridge(this, target, attrs[i], false, true);
@@ -76,113 +76,43 @@ class Module
 	end
 
 	%x{
-		function define_method_bridge(klass, target, id, name) {
-			define_method(klass, id, function() {
-				var real_target = target;
-
-				if (target.$f & T_STRING) {
-					real_target = target[0] == '@' ? this[target.substr(1)] : this[$opal.mid_to_jsid(target)].apply(this, null);
-				}
-
-				return real_target.apply(real_target, arguments);
-			});
+		function define_singleton_method_bridge (object, target, id, name) {
+			$opal.defs(object, id, method_bridge_generator(target, name));
 		}
 	}
 
 	def define_method_bridge (object, name, ali = nil)
-		`define_method_bridge(this, object, $opal.mid_to_jsid(#{ali || name}), name)`
+		%x{
+			$opal.defn(self, #{ali || name}, function () {
+				if (#{Symbol === object}) {
+					object = object[0] == '@' ? self[object.substr(1)] : this[$opal.mid_to_jsid(object)].apply(this);
+				}
 
-		self
+				return object[name].apply(object, arguments);
+			});
+		}
+
+		nil
+	end
+end
+
+module Kernel
+	def define_singleton_method_bridge (object, name, ali = nil)
+		%x{
+			$opal.defs(self, #{ali || name}, function () {
+				if (#{Symbol === object}) {
+					object = object[0] == '@' ? self[object.substr(1)] : this[$opal.mid_to_jsid(object)].apply(this);
+				}
+
+				return object[name].apply(object, arguments);
+			});
+		}
+
+		nil
 	end
 end
 
 module Native
-	class Object
-		include Native
-		include Enumerable
-
-		def initialize (*)
-			super
-
-			%x{
-				for (var name in #@native) {
-					if (#{Opal.function? `#@native[name]`}) {
-						#{define_method_bridge @native, name}
-					}
-					else {
-						#{define_accessor_bridge @native, name}
-					}
-				}
-			}
-		end
-
-		def each
-			return enum_for :each unless block_given?
-
-			%x{
-				for (var name in #@native) {
-					#{yield Object(`name`), Object(`#@native[name]`)}
-				}
-			}
-
-			self
-		end
-
-		def each_key
-			return enum_for :each_key unless block_given?
-
-			%x{
-				for (var name in #@native) {
-					#{yield Object(`name`)}
-				}
-			}
-
-			self
-		end
-
-		def each_value
-			return enum_for :each_value unless block_given?
-
-			%x{
-				for (var name in #@native) {
-					#{yield Object(`#@native[name]`)}
-				}
-			}
-		end
-
-		def [] (name)
-			Object(`#@native[name]`)
-		end
-
-		def []= (name, value)
-			`#@native[name] = #{Opal.native?(value) ? value.to_native : value}`
-		end
-
-		def nil?
-			`#@native === null || #@native === undefined`
-		end
-
-		def inspect
-			"#<Native: #{`#@native.toString()`}>"
-		end
-
-		def to_s
-			`#@native.toString()`
-		end
-
-		def to_hash
-			Hash[to_a]
-		end
-
-		def method_missing (name, *args)
-			if Opal.function? `#@native[name]`
-				__native_send__ name, *args
-			else
-				self[name]
-			end
-		end
-	end
-
 	def self.included (klass)
 		class << klass
 			def from_native (object)
@@ -209,6 +139,117 @@ module Native
 	end
 
 	alias __native_send__ native_send
+end
+
+class Native::Object
+	include Native
+	include Enumerable
+
+	def initialize (*)
+		super
+
+		update!
+	end
+
+	def update! (name = nil)
+		unless name
+			%x{
+				for (var name in #@native) {
+					#{update!(`name`)}
+				}
+			}
+
+			return
+		end
+
+		if Opal.function? `#@native[name]`
+			define_singleton_method_bridge @native, name
+
+			if respond_to? "#{name}="
+				class << self
+					self
+				end.undef_method "#{name}="
+			end
+		else
+			class << self
+				self
+			end.attr_reader_bridge @native, name
+
+			define_singleton_method "#{name}=" do |value|
+				`#@native[name] = value`
+
+				update!(name)
+			end
+		end
+	end
+
+	def each
+		return enum_for :each unless block_given?
+
+		%x{
+			for (var name in #@native) {
+				#{yield Object(`name`), Object(`#@native[name]`)}
+			}
+		}
+
+		self
+	end
+
+	def each_key
+		return enum_for :each_key unless block_given?
+
+		%x{
+			for (var name in #@native) {
+				#{yield Object(`name`)}
+			}
+		}
+
+		self
+	end
+
+	def each_value
+		return enum_for :each_value unless block_given?
+
+		%x{
+			for (var name in #@native) {
+				#{yield Object(`#@native[name]`)}
+			}
+		}
+	end
+
+	def [] (name)
+		value = `#@native[name]`
+
+		return if Opal.undefined? value
+
+		Object(value)
+	end
+
+	def []= (name, value)
+		value = value.to_native unless Opal.native?(value)
+
+		`#@native[name] = #{value}`
+
+		update!(name)
+
+		value
+	end
+
+	def nil?
+		`#@native === null || #@native === undefined`
+	end
+
+	def inspect
+		"#<Native: #{`#@native.toString()`}>"
+	end
+
+	def to_s
+		`#@native.toString()`
+	end
+
+	def to_hash
+		Hash[to_a]
+	end
 end
 
 class Object
@@ -252,36 +293,36 @@ class MatchData
 end
 
 class NilClass
-	def to_native
-		`undefined`
+	def to_native (result = undefined)
+		result
 	end
 end
 
 class Numeric
 	def to_native
-		`self.valueOf()`
+		`this.valueOf()`
 	end
 end
 
 class Proc
 	def to_native
 		%x{
-			return function () {
+			return (function () {
 				return self.apply(self.$S, arguments);
-			};
+			});
 		}
 	end
 end
 
 class Regexp
 	def to_native
-		`self.valueOf()`
+		`this.valueOf()`
 	end
 end
 
 class String
 	def to_native
-		`self.valueOf()`
+		`this.valueOf()`
 	end
 end
 
